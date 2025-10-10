aad21e23f683edfdd3db693bef587d6f000c06c3
Vedant <borodevedant4@gmail.com>
2025-10-10 17:55:36 +0530

    feat(cors): add CORS configuration to allow frontend access

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,22 @@
 import express from "express";
 import 'dotenv/config'
 import { router } from "./routes/index.routes.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 8001;
+const FrontendURL = process.env.FrontendURL|| "http://localhost:3000"
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.use(cors({
+    origin: FrontendURL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true 
+}));
+
 
 app.use("/api/v1", router)
 
